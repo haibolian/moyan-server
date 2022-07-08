4e56be5392ee6b9987787ba248bc171315fcaf56
lianhaibo <957377783@qq.com>
2022-07-08 20:47:09 +0800

    上传

--- src/controller/upload.controller.js
+++ src/controller/upload.controller.js
@@ -3,7 +3,8 @@ const { uploadAvatar } = require('../service/upload.service');
 class UploadController {
   async uploadAvatar(ctx) {
     const { body, files } = ctx.request
-    const res = uploadAvatar(body, files)
+    body._userId = ctx.state.user.id
+    const res = await uploadAvatar(body, files)
     ctx.body = {
       success: true,
       message: '上传成功',
--- src/service/upload.service.js
+++ src/service/upload.service.js
@@ -1,17 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const User = require('../model/user.model');
 
 class UploadService {
-  uploadAvatar(body, files) {
+  async uploadAvatar(body, files) {
     const file = files.avatar
-    const fileName = new Date().getTime() + file.name
+    const fileName = new Date().getTime() + '_' + file.name
+    const fileUrl = `/avatars/${fileName}`
     const reader = fs.createReadStream(file.path)
     const filePath = path.join(__dirname, '../upload/avatars/', fileName)
     const writer = fs.createWriteStream(filePath);
     reader.pipe(writer)
+    const res =  await User.update({ avatar: fileUrl }, { where: { id: body._userId } })
+    console.log(res);
     return {
+      res,
       filePath,
-      fileUrl: `/upload/avatars/${fileName}`,
+      fileUrl
     }
   }
 }
